6801b7e31283484e4a666fc4e6b00c4c66d7c465
utsav3991 <utsav3991@gmail.com>
2020-09-25 11:26:08 +0545

    refactor: update settings and env

--- app/env.py
+++ app/env.py
@@ -1,1 +1,2 @@
 DB_URL = "postgres://codavatar:helmet@localhost:5432/codavatar"
+SECRET = "123!@#^&"
--- app/settings.py
+++ app/settings.py
@@ -1,10 +1,10 @@
 from pydantic import BaseSettings
 
-from app.env import DB_URL
+from app.env import *
 
 
 class Settings(BaseSettings):
-    secret_key: str = "123!@#^&"
+    secret_key: str = SECRET
     sqlalchemy_url: str = DB_URL
 
 settings = Settings()
