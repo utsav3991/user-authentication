c01cf0bbd5fbff763900e1e4e90ea26d349d1222
utsav3991 <utsav3991@gmail.com>
2020-09-25 11:11:13 +0545

    feat: user credentials checker

---
+++ app/checkers.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Session
+
+from app import models, security
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = get_user_by_email(db=db, email=email)
+    if not user:
+        return None
+    if not security.verify_password(password, user.hashed_password):
+        return None
+    return user
