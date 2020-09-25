61977d5ee7e22d2513e5a1a1fcc4694899a2289e
utsav3991 <utsav3991@gmail.com>
2020-09-25 11:10:23 +0545

    fix: typo

--- app/models.py
+++ app/models.py
@@ -8,5 +8,5 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
-    hashed_pasword = Column(String)
+    hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
