7608af103316fd8982a743bec23bfb70af41e7c1
utsav3991 <utsav3991@gmail.com>
2020-09-25 11:24:47 +0545

    refactor: update imports

--- app/api/user.py
+++ app/api/user.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from app import schema, dependency, validators, crud
+from app import schema, dependency, validators
+from app.crud import user_crud
 
 router = APIRouter()
 
@@ -10,4 +11,4 @@ def create_user(user: schema.UserCreate, db: Session= Depends(dependency.get_db)
     db_user = validators.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
+    return user_crud.create_user(db=db, user=user)
