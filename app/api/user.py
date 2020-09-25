f520b634826cf3bf66cb0b6fc3970c5fee0e7f20
utsav3991 <utsav3991@gmail.com>
2020-09-25 11:09:45 +0545

    feat: create user

--- app/api/user.py
+++ app/api/user.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
 
-from app import schema
+from app import schema, dependency, checkers, crud
 
 router = APIRouter()
 
 @router.post("/user/", response_model=schema.User)
-def create_user(user: schema.UserCreate):
-    return {'id':1, **user.dict()}
+def create_user(user: schema.UserCreate, db: Session= Depends(dependency.get_db)):
+    db_user = checkers.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
