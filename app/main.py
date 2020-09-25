709b49c6b2e71216b00a0b172f86d24c193ec149
utsav3991 <utsav3991@gmail.com>
2020-09-25 11:10:07 +0545

    feat: authenticate router

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 
-from app.api import user
+from app.api import user, authentication
 
 app = FastAPI()
 
 app.include_router(user.router,tags=['user'])
+app.include_router(authentication.router,tags=['authenticate'])
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
