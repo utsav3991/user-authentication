25c83a34b27cf58ebf69f67a85f3daf8765fbfd3
utsav3991 <utsav3991@gmail.com>
2020-09-25 11:10:34 +0545

    feat: token model

--- app/schema.py
+++ app/schema.py
@@ -16,3 +16,12 @@ class User(UserBase):
 
     class Config:
         orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenPayload(BaseModel):
+    sub: int
